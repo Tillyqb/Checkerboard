2cc3274bfd6252a574024013537a30b7f3783ee3
tilly <tillyqb@yahoo.com>
2020-06-09 21:52:54 -0400

    Trying to display graphics

--- src/services/DisplayGraphics.java
+++ src/services/DisplayGraphics.java
@@ -8,24 +8,23 @@ public class DisplayGraphics extends Canvas
     public void paint(Graphics g)
     {
         setBackground(Color.WHITE);
-        for (int i = 0; i < 8; i++)
-        {
-            for (int j = 0; j < 8; j++)
-            {
-                if ((i+j)%2==0)
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if ((i + j) % 2 == 0)
                     setForeground(Color.RED);
                 else
                     setForeground(Color.BLACK);
-                g.fillRect((20*i),(20*j),20,20);
+                g.fillRect((40 * i), (40 * j), 40, 40);
             }
         }
     }
+
     public static void main(String[] args)
     {
-        DisplayGraphics m=new DisplayGraphics();
-        JFrame f=new JFrame();
+        DisplayGraphics m = new DisplayGraphics();
+        JFrame f = new JFrame();
         f.add(m);
-        f.setSize(200,200);
+        f.setSize(400, 400);
         f.setVisible(true);
     }
 }
